ed9e1bad8981546377ae4e8ada59be341557d797
Muhammad Sarwar <msarwardev@hotmail.com>
2019-11-19 15:32:31 +0000

    COUR-2199-Processing of the file - Back-end

---
+++ src/Dfc.ProviderPortal.FileProcessor.Functions/ApprenticeshipBulkCsvFileBlobTrigger.cs
@@ -0,0 +1,52 @@
+﻿using Dfc.ProviderPortal.FileProcessor.Provider;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Models.Models.Auth;
+
+namespace Dfc.ProviderPortal.FileProcessor.Functions
+{
+    public class ApprenticeshipBulkCsvFileBlobTrigger
+    {
+        private readonly IApprenticeshipBulkUploadService fileImporter;
+
+        public ApprenticeshipBulkCsvFileBlobTrigger(IApprenticeshipBulkUploadService fileImporter)
+        {
+            this.fileImporter = fileImporter;
+        }
+
+        [FunctionName("ApprenticeshipBulkCsvFileBlobTrigger")]
+        public async Task Run([BlobTrigger("%containerName%/{fileName}", Connection = "AzureWebJobsStorage")]Stream fileStream, string fileName, ILogger log)
+        {
+            // Hand-off all of the processing to a separate Unit of Work. 
+            // This keeps the processing independent of the trigger so that it can be called by other means.
+            CloudStorageAccount cloudStorageAccount = null;
+           
+          
+
+            string containerName = null;
+            try
+            {
+                cloudStorageAccount = GetCloudStorageAccount("AzureWebJobsStorage");
+                containerName = Environment.GetEnvironmentVariable("containerName", EnvironmentVariableTarget.Process);               
+                fileImporter.ValidateAndUploadCSV(log,fileStream, fileName);
+            }
+            catch (Exception ex)
+            {
+                log.LogCritical(ex, $"Failed to process file {fileName}");
+                await fileImporter.CreateErrorFileAsync(log, fileName, fileStream, cloudStorageAccount, containerName, $"{ex.Message} {ex.StackTrace}");
+            }
+        }
+        private CloudStorageAccount GetCloudStorageAccount(string name)
+        {
+            var connectionString = Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
+            return cloudStorageAccount;
+        }
+
+      
+    }
+}
